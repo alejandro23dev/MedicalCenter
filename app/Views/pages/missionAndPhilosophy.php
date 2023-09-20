680956c83ba0c33865b1622f8f98541d86d004a1
alejandro23dev <alejandro23dev@gmail.com>
2023-09-20 15:41:43 +0100

    update

--- app/Views/pages/missionAndPhilosophy.php
+++ app/Views/pages/missionAndPhilosophy.php
@@ -5,14 +5,14 @@
 <div class="container mt-5 text-center">
     <div>
         <h1 class="modern-title">Mission and Philosophy</h1>
-        <h2 class="modern-subtitle">Our Agency is comitted to the provision of quality services for the complete recovery
+        <h2 class="modern-subtitle">Our Agency is committed to the provision of quality services for the complete recovery
              or maximum rehabilitation of each patient. This compasses the sympathetic and understanding attitude of 
              each team member involved with patient care. The Agency feels that the patient's wellbeing is our primary concern 
              and that quality health care is a basic human need and right, our health care team exists to assist the patient. 
              In attaining state of personal health, individualized treatment is performed through effective professional assessments, 
              interventions, evaluation of specific care requirements and implementation of the therapeutic plan of the physician.
 Our Mission is to continue efforts towards to optimum resolution of problems utilizing available health resources, skills, and talents 
-of professionals, with collaboration from the patient, family and significant others</h2>
+of professionals, with collaboration from the patient, family and significant others.</h2>
     </div>
 </div>
 
